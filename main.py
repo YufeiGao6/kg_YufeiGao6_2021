2691ce19f508339dc6c965a7e9ada70ab3e63a13
Yufei Gao <ygao6@wpi.edu>
2020-03-14 19:35:20 -0400

    Added comments.

--- main.py
+++ main.py
@@ -4,24 +4,30 @@ import sys
 
 
 def main():
+    # read input s1 sand s2
     s1 = sys.argv[1]
     s2 = sys.argv[2]
+    # check if s1 or is None
     if s1 is None or s2 is None:
         print("false")
         return False
+    # check if s1 and s2 have different length
     if len(s1) != len(s2):
         print("false")
         return False
+    # store the mapping info into a dictionary
     mapping = {}
     for i in range(len(s1)):
         a = s1[i]
         b = s2[i]
         if a in mapping:
+            # print "false" since the character a cannot map to two characters
             if mapping[a] != b:
                 print("false")
                 return False
         else:
             mapping[a] = b
+    # print "true" since each character in s1 can be mapped to a character in s2
     print("true")
     return True
 
